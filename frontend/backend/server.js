8fc279a124d273f5ce8d3f0b1d457156808f2dc7
IceCola <seanderick77@gmail.com>
2024-12-05 15:46:54 +0800

    server.js

--- frontend/backend/server.js
+++ frontend/backend/server.js
@@ -1,1 +1,33 @@
-hello;
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+// mga kelangang i-install:
+// npm install express mysql cors body-parser bcrypt jsonwebtoken
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "blogdb",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log("Database connection error:", err);
+  } else {
+    console.log("Connected to MySQL");
+  }
+});
+
+app.listen(5005, () => {
+  console.log("Server running on port 5005");
+});
