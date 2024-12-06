40f8705c5ff4fbb5edfd5e08f517d65a3b9e74cb
IceCola <seanderick77@gmail.com>
2024-12-06 15:42:01 +0800

    updated

--- frontend/frontend/src/UserEnd.jsx
+++ frontend/frontend/src/UserEnd.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Login from "./Login";
 import Signup from "./Signup";
+import AdminEnd from "./AdminEnd";
 import "./App.css";
 
 function UserEnd() {
@@ -11,6 +12,8 @@ function UserEnd() {
         <Routes>
           <Route path="/" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+          {/* Use AdminEnd as a parent for all admin routes */}
+          <Route path="/adminpage/*" element={<AdminEnd />} />
         </Routes>
       </div>
     </Router>
